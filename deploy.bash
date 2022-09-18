c92234bc8e12c4b7be4a7bbd172a8dd56e8949c5
nord14541 <nord14541@gmail.com>
2022-09-18 01:18:38 +0000

    enable client cert auth

--- deploy.bash
+++ deploy.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+DIR="$1"
+kubectl create secret generic ca-secret --from-file=tls.crt=$DIR/server.crt --from-file=tls.key=$DIR/server.key --from-file=ca.crt=$DIR/ca.crt
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml
 kubectl apply -f manifests/
